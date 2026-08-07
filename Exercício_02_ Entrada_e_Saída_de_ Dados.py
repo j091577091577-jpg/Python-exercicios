804281e1932e7dd0f3f72effa3c43e45f9d0a971
j091577091577-jpg <j091577091577@gmail.com>
2026-08-07 13:38:44 +0200

    Add files via upload

---
+++ Exercício_02_ Entrada_e_Saída_de_ Dados.py
@@ -0,0 +1,16 @@
+# ============================================
+# Exercício 01 - Boas-vindas e Entrada de Dados
+# Autor: Josué
+# Linguagem: Python
+# Descrição: Primeiro programa com entrada e saída de dados.
+# ============================================
+
+print("=" * 40)
+print("      BEM-VINDO AO PYTHON")
+print("=" * 40)
+
+nome = input("Qual é o seu nome? ")
+
+print("\nOlá, {}!".format(nome))
+print("Bem-vindo ao mundo da programação.")
+print("Que este seja o início da tua jornada para te tornares um excelente desenvolvedor de software.")
